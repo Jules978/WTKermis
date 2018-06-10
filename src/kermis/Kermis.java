cd4b9a9e7c76fbf5a65f8bdcbda7929cab51ec5b
JJD <vanDrunen.julia@gmail.com>
2018-06-10 18:37:05 +0200

    first commit

---
+++ src/kermis/Kermis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kermis;
+
+/**
+ *
+ * @author Julia
+ */
+public class Kermis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
